495a287bd4ce75f9c079f5465d6b5d4ff50ca7db
k1911jp <k1911jp@k1911jp.com>
2022-05-19 00:45:05 +0900

    First

---
+++ Test/Script/K1_DebugCube.cs
@@ -0,0 +1,14 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+namespace K1911jp {
+    public class K1_DebugCube : UdonSharpBehaviour {
+        [SerializeField] private GameObject objModel;
+        public override void Interact() {
+            Debug.Log("Hello VRChat World");
+            objModel.SetActive(!objModel.activeSelf);
+        }
+    }
+}
